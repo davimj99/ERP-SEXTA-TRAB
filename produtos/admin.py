916033e1d3647245429dd448795828558beab1b9
Davi Souza <davimj99@gmail.com>
2026-04-16 18:40:17 -0300

    Commits do app produto, no admin e views

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-from .models import Produto
+from django.contrib import admin #Importa o sistema de admin do Django
+from .models import Produto #importando o model para o admin pra registrar la
 
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome',  'preco', 'ativo')
-    search_fields = ('nome',)
-    list_filter = ('ativo',)
+@admin.register(Produto) #decorator que vai registrar o model produto no admin
+class ProdutoAdmin(admin.ModelAdmin): #classe de personaliza de produto pra aparecer no admin, admin.ModelAdmin classe padrão do Django para configuração
+    list_display = ('nome',  'preco', 'ativo') #list_display Define quais colunas aparecem na lista do admin
+    search_fields = ('nome',) #search_fields Adiciona uma barra de busca
+    list_filter = ('ativo',) #list_filter Adiciona um filtro lateral no admin
+#TODAS FUNÇÃO LEMBRAM SIDEBAR E UM NAV
+
+#Serve para gerenciar dados do sistema (tipo um mini ERP interno)
--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render #junta um template HTML + dados + request e devolve para uma página pronta
 
-def lista_produtos(request):
-    return render(request, 'produtos/lista.html')
+def lista_produtos(request): #representa a requisição do usuário (quando alguém acessa a página)
+    return render(request, 'produtos/lista.html') 
+
+"""
+request → a requisição do usuário
+o arquivo HTML que será exibido
+Views são o **cérebro do sistema**.
+"""
