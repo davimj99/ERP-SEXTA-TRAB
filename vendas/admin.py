f57ea4f103c61da4e889a1ef4e02630340d4e8c9
Davi Souza <davimj99@gmail.com>
2026-04-16 17:23:25 -0300

    Atualizando modulo vendas

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Venda, ItemVenda
 
-# Register your models here.
+
+class ItemVendaInline(admin.TabularInline):
+    model = ItemVenda
+    extra = 1
+
+
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_venda', 'valor_total')
+    inlines = [ItemVendaInline]
+    readonly_fields = ('valor_total',)
+
+@admin.register(ItemVenda)
+class ItemVendaAdmin(admin.ModelAdmin):
+    list_display = ('venda', 'produto', 'quantidade', 'preco_unitario')
--- vendas/models.py
+++ vendas/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from produtos.models import Produto
 
+
 class Venda(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.IntegerField()
-    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
     data_venda = models.DateTimeField(auto_now_add=True)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Venda {self.id}"
+
+    def calcular_total(self):
+        total = sum(item.subtotal() for item in self.itens.all())
+        self.valor_total = total
+        self.save()
+
+    def baixar_estoque(self):
+        for item in self.itens.all():
+            produto = item.produto
+            produto.estoque -= item.quantidade
+            produto.save()
+
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name="itens")
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def subtotal(self):
+        return self.quantidade * self.preco_unitario
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.venda.calcular_total()
