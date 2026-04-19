03ceb7df9f22b19f4813bfe6da29b6d3d97b1c48
Jonathanls19 <jonathaan416@gmail.com>
2026-04-19 18:25:41 -0300

    Add files via upload

---
+++ clientes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Isso vai fazer a rota /clientes/ funcionar
+    path('', views.clientes, name='clientes'), 
+    path('novo/', views.novo_cliente, name='novo_cliente'),
+    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('excluir/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
+]
