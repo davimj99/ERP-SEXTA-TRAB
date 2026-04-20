de1dc3c988f2a4f277364b5cb58114e7c81eaab5
Davi Souza <davimj99@gmail.com>
2026-04-20 17:38:37 -0300

    Validando templates clientes, alterando nome de rota pra pt-br, e modificando rotas

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # Isso vai fazer a rota /clientes/ funcionar
     path('', views.clientes, name='clientes'), 
-    path('new/', views.novos_clientes, name='novos_clientes'),
+    path('novo/', views.novos_clientes, name='novos_clientes'),
     path('edit/<int:id>/', views.editar_clientes, name='editar_clientes'),
     path('excluir/<int:id>/', views.excluir_clientes, name='excluir_clientes'),
 ]
