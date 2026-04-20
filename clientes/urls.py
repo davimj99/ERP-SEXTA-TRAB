8f636628595dfad7e97651658b11722db87b2c42
Jonathanls19 <jonathaan416@gmail.com>
2026-04-20 12:34:16 -0300

    Alguns ajustes no arquivo urls.py

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Isso vai fazer a rota /clientes/ funcionar
-    path('', views.clientes, name='clientes'), 
-    path('novo/', views.novo_cliente, name='novo_cliente'),
-    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
-    path('excluir/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
-] 
+    # Isso vai fazer a rota /clients/ funcionar
+    path('', views.clients, name='clients'), 
+    path('new/', views.new_clients, name='new_clients'),
+    path('edit/<int:id>/', views.edit_clients, name='edit_clients'),
+    path('excluir/<int:id>/', views.excluir_clients, name='excluir_clients'),
+]
