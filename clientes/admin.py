f513b7f6851752b40f0de7c91d5104b9292e1692
Jonathanls19 <jonathaan416@gmail.com>
2026-04-20 12:46:00 -0300

    Ajustes

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Cliente
 
-@admin.register(Cliente)
+admin.site.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cpf', 'telefone')
     search_fields = ('nome', 'cpf')
