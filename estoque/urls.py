472c67aa1f8112dac9741f8097bafa39f9f04e4d
Jonathanls19 <jonathaan416@gmail.com>
2026-04-20 16:02:13 -0300

    Ajustes na urls.py

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path # A função path, que é usada para criar rotas URLs no Django.
-from .views import lista_estoque, criar_estoque #Importa as funções da sua views.py, receber a requisição do usuário/processa dados/retornar uma página HTML
+from django.urls import path
+from .views import estoque, novo_estoque
 
-urlpatterns = [ #Lista onde você define todas as rotas desse app.
-    path('', lista_estoque, name='estoque'), #'' → URL vazia (ex: /estoque/), lista_estoque : função executada / name='estoque' : nome interno da rota
-    path('criar/', criar_estoque, name='criar_estoque'), #'criar/'  URL: /estoque/criar/ ; criar_estoque : função chamada / nome da rota =  name='criar_estoque' 
+urlpatterns = [
+    # A rota principal que abre a lista de estoque
+    path('', estoque, name='estoque'),
+    
+    # A rota que abre o formulário para registrar nova entrada
+    path('new/', novo_estoque, name='novo_estoque'), #'criar/'  URL: /estoque/criar/ ; criar_estoque : função chamada / nome da rota =  name='criar_estoque' 
 ]
 
 #http://127.0.0.1:8000/estoque/  ou localhost:8000/estoque
