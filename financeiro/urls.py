8e668ff78eb3832dc3035bb2a6eddd96cc69d438
DAVID LUIZ SOUZA NASCIMENTO <davidnascimento999@cs.udf.edu.br>
2026-04-24 10:19:00 -0300

    alterando url modulo financeiro

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista_financeiro
+from .views import lista_financeiro, lista_transacoes
 
 urlpatterns = [
     path('', lista_financeiro, name='financeiro'),
+    path('', lista_transacoes, name='lista_transacoes'),
 ]
--- financeiro/views.py
+++ financeiro/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Transacao
 
+def lista_transacoes(request):
+    transacoes = Transacao.objects.all().order_by('-data')
+    return render(request, 'lista.html', {'transacoes': transacoes})
 def lista_financeiro(request):
-    return render(request, 'financeiro/lista.html')
+    return render(request, 'lista.html')
